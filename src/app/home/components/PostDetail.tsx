222c3ea1fa8438317640bd628875049e3428b0c7
jeonjunbum <184849896+jeonjunbum@users.noreply.github.com>
2026-08-11 02:46:19 +0900

    feat(report): 신고 API 프론트엔드 연동

--- src/app/home/components/PostDetail.tsx
+++ src/app/home/components/PostDetail.tsx
@@ -542,6 +542,8 @@ export function PostDetail({ postId, onBack, initialScrollTarget, onPostUpdated,
 
       {reportComment ? (
         <ReportContentModal
+          targetType="COMMENT"
+          targetId={reportComment.commentId}
           title="댓글 신고"
           author={reportComment.author}
           authorHandle={reportComment.authorHandle}
@@ -552,6 +554,8 @@ export function PostDetail({ postId, onBack, initialScrollTarget, onPostUpdated,
       ) : null}
       {reportPostOpen ? (
         <ReportContentModal
+          targetType="POST"
+          targetId={post.postId}
           title="게시물 신고"
           author={post.author}
           authorHandle={post.authorHandle}
